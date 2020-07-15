fe1e980449706210875f3ad0ae573ae8c19cae23
Jiarui XU <xvjiarui0826@gmail.com>
2020-07-16 02:19:35 +0800

    add MMSyncBN setting

---
+++ configs/batch8/deeplabv3plus_r101-d8_512x1024_80k_cityscapes.py
@@ -0,0 +1,8 @@
+_base_ = './deeplabv3plus_r50-d8_512x1024_80k_cityscapes.py'
+norm_cfg = dict(type='MMSyncBN', requires_grad=True)
+model = dict(
+    pretrained='open-mmlab://resnet101_v1c',
+    backbone=dict(depth=101, norm_cfg=norm_cfg),
+    decode_head=dict(norm_cfg=norm_cfg),
+    auxiliary_head=dict(norm_cfg=norm_cfg))
+data = dict(samples_per_gpu=1, workers_per_gpu=2)
