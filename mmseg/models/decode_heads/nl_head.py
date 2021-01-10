2c221f86655858fff97d00080538283b0885fc6d
Jerry Jiarui XU <xvjiarui0826@gmail.com>
2021-01-09 19:08:53 -0800

    Update nl_head.py

--- mmseg/models/decode_heads/nl_head.py
+++ mmseg/models/decode_heads/nl_head.py
@@ -7,18 +7,6 @@ from .fcn_head import FCNHead
 
 @HEADS.register_module()
 class NLHead(FCNHead):
-    """Non-local Neural Networks.
-
-    This head is the implementation of `NLNet
-    <https://arxiv.org/abs/1711.07971>`_.
-
-    Args:
-        reduction (int): Reduction factor of projection transform. Default: 2.
-        use_scale (bool): Whether to scale pairwise_weight by
-            sqrt(1/inter_channels). Default: True.
-        mode (str): The nonlocal mode. Options are 'embedded_gaussian',
-            'dot_product'. Default: 'embedded_gaussian.'.
-    """
 
     def __init__(self,
                  reduction=2,
@@ -38,12 +26,6 @@ class NLHead(FCNHead):
             mode=self.mode)
 
     def forward(self, inputs):
-        """Forward function."""
-        x = self._transform_inputs(inputs)
-        output = self.convs[0](x)
-        output = self.nl_block(output)
-        output = self.convs[1](output)
-        if self.concat_input:
-            output = self.conv_cat(torch.cat([x, output], dim=1))
-        output = self.cls_seg(output)
-        return output
+        """Forward function. Omitted Here"""
+        pass
+        
